2c6b774c91881df5f0ab292f8606b68ce10ef457
KongzhangHao <haokongzhang@gmail.com>
2017-12-27 16:14:32 +0800

    Add conditions to the movement of player, including dye, dyedSlime and wall.

--- src/Hero.java
+++ src/Hero.java
@@ -16,10 +16,16 @@ public class Hero {
 	
 	// Move
 	/*
+	 * check if destination movable
 	 * change original position to original object
-	 * move position to new position
+	 * if on slime, then set the position to be slime, else ground
+	 * if new position has slime on it, then set onSlime to be true
+	 * set new position to player
+	 * update player's position
 	 */
 	public void moveUp() {
+		int[] destination = {position[0], position[1] + 1};
+		if (!movable(destination)) return; 
 		if (onSlime) map.setPosition(position, GameObject.slime);
 		else map.setPosition(position, GameObject.ground);
 		position[1]++;
@@ -29,6 +35,8 @@ public class Hero {
 	}
 	
 	public void moveDown() {
+		int[] destination = {position[0], position[1] - 1};
+		if (!movable(destination)) return; 
 		if (onSlime) map.setPosition(position, GameObject.slime);
 		else map.setPosition(position, GameObject.ground);
 		position[1]--;
@@ -38,6 +46,8 @@ public class Hero {
 	}
 	
 	public void moveLeft() {
+		int[] destination = {position[0] - 1, position[1]};
+		if (!movable(destination)) return; 
 		if (onSlime) map.setPosition(position, GameObject.slime);
 		else map.setPosition(position, GameObject.ground);
 		position[0]--;
@@ -47,6 +57,8 @@ public class Hero {
 	}
 	
 	public void moveRight() {
+		int[] destination = {position[0] - 1, position[1]};
+		if (!movable(destination)) return; 
 		if (onSlime) map.setPosition(position, GameObject.slime);
 		else map.setPosition(position, GameObject.ground);
 		position[0]++;
@@ -54,6 +66,42 @@ public class Hero {
 		if (map.getPosition(position) == GameObject.slime) onSlime = true;
 		map.setPosition(position, GameObject.player);
 	}
+	
+	// helper function, check if destination movable
+	/*
+	 * if the destination is ground or slime, can go straight away.
+	 * if the destination is dye or dyedSlime, need to check the next step
+	 * if next step is ground or slime, then move the dye to the right place
+	 */
+	private boolean movable(int[] destination) {
+		boolean ret = false;
+		if (map.getPosition(destination) == GameObject.ground 
+				|| map.getPosition(destination) == GameObject.slime) {
+			ret = true;
+		}
+		if (map.getPosition(destination) == GameObject.dye
+			|| map.getPosition(destination) == GameObject.dyedSlime) {
+			int[] nextStep = {2 * destination[0] - position[0]
+					, 2 * destination[1] - position[1]};
+			if (map.getPosition(nextStep) == GameObject.ground 
+					|| map.getPosition(nextStep) == GameObject.slime) {
+				ret = true;
+				if (map.getPosition(nextStep) == GameObject.slime) {
+					map.setPosition(nextStep, GameObject.dyedSlime);
+				} else {
+					map.setPosition(nextStep, GameObject.dye);
+				}
+				if (map.getPosition(destination) == GameObject.dye) {
+					map.setPosition(destination, GameObject.ground);
+				} else {
+					map.setPosition(destination, GameObject.slime);
+				}
+			}
+		}
+		
+		return ret;
+	}
+	
 	
 	// Setters and Getters
 	public int[] getPosition() {
