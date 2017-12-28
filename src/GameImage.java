3a8a5fde7d01e06195980c312f87576f1d216090
KongzhangHao <haokongzhang@gmail.com>
2017-12-28 10:13:00 +0800

    updated the constructor to load images from the file system. created getImg method to get image.

--- src/GameImage.java
+++ src/GameImage.java
@@ -6,12 +6,13 @@ import java.awt.Toolkit;
  * 		  for objects, buttons and status bar.
  * @author hao
  * @file GameImage.java
- * @date 28/12/2017 hao: created GameImage class to handle image loading and usage
+ * @date 28/12/2017 hao: updated the constructor to load images from the file system.
+ * 						 created getImg method to get image.
  */
 public class GameImage {
 	
-	private static Image[] imgs; /**< buffer storing all the object images*/
-	private static Toolkit tk; /**< toolkit used to get the image*/
+	private Image[] imgs; /**< buffer storing all the object images*/
+	private Toolkit tk; /**< toolkit used to get the image*/
 	
 	
 	/**
@@ -19,6 +20,43 @@ public class GameImage {
 	 *        sets their size using toolkit.
 	 */
 	public GameImage() {
+		tk = Toolkit.getDefaultToolkit();
 		
+		/** Load the images from the file system to the buffer using toolkit */
+		imgs = new Image[] {
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_magma.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/wall.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/floor.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_dye.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/green_slime.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_role_face.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_role_back.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_role_right.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_role_left.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/red_slime.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/pickaxe.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/stairs.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/witch_face.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/witch_left.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/witch_right.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/witch_back.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/fire.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/ice.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/thunder.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/pickaxe_effect.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/ulti.gif")),
+				tk.getImage(GameFrame.class.getClassLoader().getResource("imgs/kill_slime.gif"))
+		};
+	}
+	
+	
+	/**
+	 * @brief Get the image of the game object
+	 * @param object the index of the game object 
+	 * @return the image of the object
+	 * @precondition The object index is valid
+	 */
+	public Image getImg(int object) {
+		return imgs[object];
 	}
 }
