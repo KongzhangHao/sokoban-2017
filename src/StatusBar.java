0aee1234d3de3330c5abc6d8042adcb778eca060
KongzhangHao <haokongzhang@gmail.com>
2017-12-28 10:42:14 +0800

    Created StatusBar.java.

---
+++ src/StatusBar.java
@@ -0,0 +1,37 @@
+
+/**
+ * @brief The functions linked to the buttons in the status bar
+ * @author hao
+ * @file StatusBar.java
+ * @date 28/12/2017 hao: Created StatusBar.java.
+ */
+public class StatusBar {
+	
+	public StatusBar(GameEngine game) {
+		
+	}
+	
+	public void goBack() {
+		
+	}
+	
+	public void goNext() {
+		
+	}
+
+	public void back() {
+	
+	}
+	
+	public void reStart() {
+		
+	}
+	
+	public void choice(int level) {
+		
+	}
+	
+	public void setLevel(int level) {
+		
+	}
+}
