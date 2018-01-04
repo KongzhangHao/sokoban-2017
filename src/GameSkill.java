50ec7933024a6359ec8f505c2b85eb53d589de5d
KongzhangHao <haokongzhang@gmail.com>
2018-01-04 16:19:14 +0800

    created GameSkill.java

---
+++ src/GameSkill.java
@@ -0,0 +1,14 @@
+
+/**
+ * @brief The skill that heroes can use, with different purposes and different effects
+ * @author hao
+ * @file GameSkill.java
+ * @date 04/01/2018 hao: created GameSkill.java
+ */
+public class GameSkill {
+
+	public GameSkill() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
