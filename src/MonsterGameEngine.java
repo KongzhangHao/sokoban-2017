3dff9bb98a5d2c7efe5c1a652d61eaeb92de420a
KongzhangHao <haokongzhang@gmail.com>
2018-01-04 12:23:59 +0800

    Display "Game Over" when both heroes are killed by the monster

--- src/MonsterGameEngine.java
+++ src/MonsterGameEngine.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
 /**
@@ -6,7 +9,7 @@ import java.awt.event.KeyEvent;
  * @file MonsterGameEngine.java
  * @date 03/01/2018 hao: Created MonsterGameEngine.java
  * 					hao: Added Monster movement when player moves
- *
+ *		 04/01/2018 hao: Display "Game Over" when both heroes are killed by the monster
  */
 public class MonsterGameEngine extends MultiPlayerGameEngine {
 	
@@ -24,6 +27,38 @@ public class MonsterGameEngine extends MultiPlayerGameEngine {
 		enemy = new Monster(super.getMap());
 	}
 	
+	/** 
+	 * @brief Display the objects from 2d map onto the panel using images.
+	 * 		  Display "Game Over" when both heroes are killed by the monster
+	 * @see javax.swing.JComponent#paint(java.awt.Graphics)
+	 */
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+		/** Display GameOver if game has ended */
+		if (gameOver()) {
+			g.setColor(Color.white);
+			g.setFont(new Font("Garamond", Font.BOLD, 80));
+			g.drawString("Game Over", 80, 220);
+			g.setFont(new Font("Garamond", Font.BOLD, 80));
+			g.drawString("You DIE!", 130, 320);
+		}
+	}
+	
+	/**
+	 * @brief Check if the current game is over. "Over" means that the 
+	 * 		  two heroes are both killed in this case.
+	 * @return true current game is over
+	 * @return false current game is not over
+	 */
+	protected boolean gameOver() {
+		
+		/** If both heroes are killed */
+		if (!super.getHero().isAlive() && !super.getHeroine().isAlive()) return true;
+		return false;
+	}
+
 	/**
 	 * @brief Detect the pressed key and set each key as instruction of
 	 * characters' movement
@@ -39,6 +74,12 @@ public class MonsterGameEngine extends MultiPlayerGameEngine {
 		updateAliveStatus();
 	}
 	
+	/**
+	 * @brief Check that the key pressed is valid. Check the hero controlled by a key is still alive
+	 * @param keyCode the key pressed by the player
+	 * @return true The key pressed is valid
+	 * @return false The key pressed is not valid
+	 */
 	protected boolean validKeyPress(int keyCode) {
 		if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_DOWN 
 				|| keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_RIGHT) {
@@ -79,6 +120,9 @@ public class MonsterGameEngine extends MultiPlayerGameEngine {
 		enemy.locateHero();
 	}
 
+	/**
+	 * @brief Scan through the map to update the heroes' latest alive status
+	 */
 	protected void updateAliveStatus() {
 		/** Check if the object on the hero's position is the player */
 		if (!GameObject.isPlayer(getMap().getPosition(super.getHero().getPosition()))) {
