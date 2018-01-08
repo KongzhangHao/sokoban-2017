16225ee16917bbecfe4a443c2f193e55bafdbea9
KongzhangHao <haokongzhang@gmail.com>
2018-01-08 16:27:36 +0800

    Fixed Bug: Skill effect remains after the skill is finished

--- src/GameSkill.java
+++ src/GameSkill.java
@@ -2,12 +2,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
 /**
  * @brief The skill that heroes can use, with different purposes and different effects
  * @author hao
  * @file GameSkill.java
  * @date 04/01/2018 hao: created GameSkill.java
  * 					hao: Added flash method to flash the skill on one block using timer
+ * 					hao: Fixed Bug: Skill effect remains after the skill is finished
  */
 public class GameSkill {
 
@@ -46,24 +49,61 @@ public class GameSkill {
 	}
 	
 	/**
+	 * @brief Cast the skill by given the hero's position who used this skill
+	 * @param heroPosition the hero's position on map
+	 */
+	public void castSkill(int[] heroPosition) {
+		int heroPosX = heroPosition[0];
+		int heroPosY = heroPosition[1];
+		if (game.getMap().getPosition(heroPosX, heroPosY) == GameObject.warriorFront
+				|| game.getMap().getPosition(heroPosX, heroPosY) == GameObject.witchFront) {
+			flash(heroPosX, heroPosY + 1);
+		} else if (game.getMap().getPosition(heroPosX, heroPosY) == GameObject.warriorBack
+					|| game.getMap().getPosition(heroPosX, heroPosY) == GameObject.witchBack) {
+			flash(heroPosX, heroPosY - 1);
+		} else if (game.getMap().getPosition(heroPosX, heroPosY) == GameObject.warriorLeft
+					|| game.getMap().getPosition(heroPosX, heroPosY) == GameObject.witchLeft) {
+			flash(heroPosX - 1, heroPosY);
+		} else if (game.getMap().getPosition(heroPosX, heroPosY) == GameObject.warriorRight
+					|| game.getMap().getPosition(heroPosX, heroPosY) == GameObject.witchRight) {
+			flash(heroPosX + 1, heroPosY);
+		}
+	}
+	
+	
+	/**
 	 * @brief Cast the skill at a single block, flash for once
 	 * @param posX x-axis of skill position
 	 * @param posY y-axis of skill position
-	 * @param code1 image which the square should be after the action
-	 * @param code2 animation image for the action
-	 * @param flash_limit times of flash
 	 */
 	public void flash(int posX,int posY){
 		
-		ActionListener taskPerformer = new ActionListener(){
+		/** Get the action to perform in each round of flashing */
+		ActionListener taskPerformer = flashAction(posX, posY);
+		
+		/** Set and start the timer to perform the flash action */
+		Timer timer = new Timer(400, taskPerformer);
+		timer.setRepeats(true);
+		timer.start();
+	}
+	
+	/**
+	 * @brief Get the action to perform in each round of flashing
+	 * @param posX x axis of the place to flash
+	 * @param posY y axis of the place to flash
+	 * @return the action to perform in flashing
+	 */
+	protected ActionListener flashAction(int posX, int posY) {
+		
+		ActionListener taskPerformer = new ActionListener() {
 			
 			private int prevObject; /**< The previous game object on the position */
 			
-			public void actionPerformed(ActionEvent e){
+			public void actionPerformed(ActionEvent e) {
 				flashNum++;
 				
 				/** If all the flash rounds are done, then stop the timer */
-				if (flashNum >= flashRounds * 2){
+				if (flashNum > flashRounds * 2){
 					flashNum = 0;
 					Timer timer = (Timer)e.getSource();
 					timer.stop();
@@ -75,7 +115,7 @@ public class GameSkill {
 				}
 				
 				/** Set the game object on the position according to the skill and previous image */
-				if (flashNum % 2 == 0) {
+				if (flashNum % 2 == 1) {
 					game.getMap().setPosition(posX, posY, skillImage);
 				} else {
 					game.getMap().setPosition(posX, posY, afterSkill(prevObject));
@@ -85,9 +125,7 @@ public class GameSkill {
 			}
 		};
 		
-		Timer timer = new Timer(400,taskPerformer);
-		timer.setRepeats(true);
-		timer.start();
+		return taskPerformer;
 	}
 	
 	/**
@@ -98,6 +136,4 @@ public class GameSkill {
 	protected int afterSkill(int object) {
 		return object;
 	}
-	
-
 }
