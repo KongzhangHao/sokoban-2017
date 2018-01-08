cbcc60d0ca54d4a8dd8b21d9e2b7c8786712c838
KongzhangHao <haokongzhang@gmail.com>
2018-01-08 15:29:22 +0800

    Created MonsterGameEngineWithSkill.java

---
+++ src/MonsterGameEngineWithSkill.java
@@ -0,0 +1,14 @@
+
+/**
+ * @brief The engine for the game with Monster AI and player hero skills available
+ * @author hao
+ * @file MonsterGameEngineWithSkill.java
+ * @date 08/01/2018 hao: Created MonsterGameEngineWithSkill.java
+ */
+public class MonsterGameEngineWithSkill extends MonsterGameEngine {
+
+	public MonsterGameEngineWithSkill() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
