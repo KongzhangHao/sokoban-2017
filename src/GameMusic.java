9e6dbcbe69fce5a3b6cc26ffd9edd0f20ca63ca0
KongzhangHao <haokongzhang@gmail.com>
2018-01-10 11:22:46 +0800

    Added method to load music from file path

--- src/GameMusic.java
+++ src/GameMusic.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
 
 /**
  * @brief Music player which controls the music in the game
@@ -8,8 +11,45 @@
  */
 public class GameMusic {
 
-	public GameMusic() {
-		// TODO Auto-generated constructor stub
+	private Clip bgm; /**< background music of the game */
+	private Clip gameOverMusic; /**< game over music */
+	
+	/**
+	 * @brief Constructor, loads the bgm and gameOverMusic from the given file path.
+	 * @param bgmPath file path of background music
+	 * @param gameOverMusicPath file path of game over music
+	 */
+	public GameMusic(String bgmPath, String gameOverMusicPath) {
+		this.bgm = loadFromPath(bgmPath);
+		this.gameOverMusic = loadFromPath(gameOverMusicPath);
 	}
+
+	/**
+	 * @brief Load the specific music and return it in Clip style
+	 * @param musicPath file path of the music
+	 * @return Clip type of the music
+	 */
+	private Clip loadFromPath(String musicPath) {
+		AudioInputStream audio;
+		Clip clip = null;
+		
+		try {
+			
+			/** load music with given path */
+			audio = AudioSystem.getAudioInputStream(new File(musicPath).getAbsoluteFile());
+			clip = AudioSystem.getClip();
+			clip.open(audio);
+			
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		return clip;
+	}
+	
+	
 
 }
