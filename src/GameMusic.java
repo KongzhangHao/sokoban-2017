20a40e3531af4c13b760369c93f69b0d6e700247
KongzhangHao <haokongzhang@gmail.com>
2018-01-10 13:49:38 +0800

    Fixed bug: Trying to run a music while it's already running

--- src/GameMusic.java
+++ src/GameMusic.java
@@ -7,7 +7,8 @@ import javax.sound.sampled.*;
  * @author hao
  * @file GameMusic.java
  * @date 10/01/2018	hao: Created GameMusic.java
- *
+ *					hao: Added method to load music from file path
+ *					hao: Fixed bug: Trying to run a music while it's already running
  */
 public class GameMusic {
 
@@ -50,6 +51,56 @@ public class GameMusic {
 		return clip;
 	}
 	
+	/**
+	 * @brief Start playing the bg music
+	 */
+	public void playBGM() {
+		if (bgm.isRunning()) return;
+		
+		/** Stop the game over music if it is runinng */
+		if (gameOverMusic.isRunning()) gameOverMusic.stop();
+		
+		/** Start playing the bg music */
+		if (!bgm.isRunning()) bgm.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+
+	/**
+	 * @brief Start playing the gameOver music
+	 */
+	public void playGameOverMusic() {
+		if (gameOverMusic.isRunning()) return;
+		
+		/** Stop the bgm if it is runinng */
+		if (bgm.isRunning()) bgm.stop();
+		
+		/** Start playing the game over music */
+		gameOverMusic.start();
+	}
+	
+	/**
+	 * @brief Play the music as given in the file path
+	 * @param filePath give music's file path
+	 */
+	public void playSound(String filePath) {
+	    AudioInputStream audio;
+	    
+		try {
+			
+			/** load music with given path */
+			audio = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audio);
+			clip.start();
+			
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+
+	}
 	
 
 }
