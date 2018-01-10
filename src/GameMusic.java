41e48a94ac3f588782ada80dc6ac49401ce4532a
KongzhangHao <haokongzhang@gmail.com>
2018-01-10 11:14:12 +0800

    Created GameMusic.java

---
+++ src/GameMusic.java
@@ -0,0 +1,15 @@
+
+/**
+ * @brief Music player which controls the music in the game
+ * @author hao
+ * @file GameMusic.java
+ * @date 10/01/2018	hao: Created GameMusic.java
+ *
+ */
+public class GameMusic {
+
+	public GameMusic() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
